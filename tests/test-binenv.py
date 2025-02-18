48de4ba0ad8423df4f90d388b4cd738c3f92c600
Gunther Klessinger <gunther.klessinger@axiros.com>
2025-02-18 22:02:19 +0100

    elf

--- tests/test-binenv.py
+++ tests/test-binenv.py
@@ -2,24 +2,43 @@ import yaml
 import requests
 import os, sys
 
+H = os.environ["HOME"]
 # os.environ["PATH"] = ":".join([os.environ["HOME"], os.environ["PATH"]])
-fn_distris = os.environ["HOME"] + "/.config/binenv/distributions.yaml"
+fn_distris = H + "/.config/binenv/distributions.yaml"
+
+
+def checknonelf(k):
+    d = H + "/.binenv/binaries/" + k
+    if not os.path.exists(d):
+        return f"missing {d}"
+    for k in os.listdir(d):
+        if not b"ELF" in open(d + "/" + k, "rb").read(4):
+            return str(open(d + "/" + k, "rb").read(20))
+        return
+    return "no files"
 
 
 def main():
     with open(fn_distris) as f:
         data = yaml.safe_load(f)
     uninst = []
-    for k, spec in data["sources"].items():
+    nonelf = []
+    for k, spec in data["sources"].items()[:10]:
         b = spec["install"].get("binaries")
         print("🟩 testing", k, b, file=sys.stderr)
-        if os.system("binenv install " + k):
-            print("🟠 install failed", k)
-            uninst.append(k)
 
-        # elif os.system(k + " --version"):
-        #     print("🟥 failed", k)
+        if os.system("binenv install " + k):
+            print(f"🟠 {k} install failed")
+            uninst.append(k)
+            continue
+        e = checknonelf(k)
+        if e:
+            print(f"🟤 {k}: failed ELF check", b)
+            nonelf.append(k)
+
     print("uninstallable", uninst, file=sys.stderr)
+    print()
+    print("non ELF", nonelf, file=sys.stderr)
 
 
 if __name__ == "__main__":
