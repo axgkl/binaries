f10b21d84fab14b08f72a29616bfcdaa96445c04
Gunther Klessinger <gunther.klessinger@axiros.com>
2025-02-18 21:10:14 +0100

    testing stuff for real

--- tests/test-binenv.py
+++ tests/test-binenv.py
@@ -1,12 +1,20 @@
 import yaml
 import requests
 import os
+
 # os.environ["PATH"] = ":".join([os.environ["HOME"], os.environ["PATH"]])
+fn_distris = os.environ["HOME"] + "/.config/binenv/distributions.yaml"
 
 
 def main():
-    print("Hello, World!")
-    os.system("binenv")
+    with open(fn_distris) as f:
+        data = yaml.safe_load(f)
+    for k, spec in data["sources"].items():
+        b = spec["install"].get("binaries")
+        print("🟩 testing", k, b)
+        os.system("binenv install " + k)
+        if os.system(k):
+            print("🟥 failed", k)
 
 
 if __name__ == "__main__":
