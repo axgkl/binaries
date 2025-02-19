002c1d32b0e124514ccfd02007073cd013b7b1b0
Gunther Klessinger <gunther.klessinger@axiros.com>
2025-02-19 09:08:01 +0100

    idempotent

--- binenv-install.sh
+++ binenv-install.sh
@@ -9,6 +9,7 @@ rm binenv
 rm checksums.txt
 if [[ -n $BASH ]]; then ZESHELL=bash; fi
 if [[ -n $ZSH_NAME ]]; then ZESHELL=zsh; fi
+type binenv 2>/dev/null && exit 0
 echo -e '\nexport PATH=~/.binenv:$PATH' >>~/".${ZESHELL}rc"
-echo "source <(binenv completion ${ZESHELL})" >>~/".${ZESHELL}rc"
 . ~/".${ZESHELL}rc"
+#echo "source <(binenv completion ${ZESHELL})" >>~/".${ZESHELL}rc"
