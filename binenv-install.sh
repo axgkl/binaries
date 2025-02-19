55b5c0a0540f783f680138747e5fb4d462550336
Gunther Klessinger <gunther.klessinger@axiros.com>
2025-02-19 08:28:36 +0100

    checksums

--- binenv-install.sh
+++ binenv-install.sh
@@ -1,11 +1,14 @@
 wget -q https://github.com/devops-works/binenv/releases/download/v0.19.11/binenv_linux_amd64
+wget -q https://github.com/devops-works/binenv/releases/download/v0.19.11/checksums.txt
+sha256sum --check --ignore-missing checksums.txt
 mv binenv_linux_amd64 binenv
 chmod +x binenv
 ./binenv update
 ./binenv install binenv
 rm binenv
+rm checksums.txt
 if [[ -n $BASH ]]; then ZESHELL=bash; fi
 if [[ -n $ZSH_NAME ]]; then ZESHELL=zsh; fi
 echo -e '\nexport PATH=~/.binenv:$PATH' >>~/".${ZESHELL}rc"
 echo "source <(binenv completion ${ZESHELL})" >>~/".${ZESHELL}rc"
-exec "$SHELL"
+. ~/".${ZESHELL}rc"
