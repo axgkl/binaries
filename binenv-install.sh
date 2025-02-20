b5f2374d1c546d84a2e91edbc689b4255cae1531
Gunther Klessinger <gunther.klessinger@axiros.com>
2025-02-20 12:33:58 +0100

    better inst

--- binenv-install.sh
+++ binenv-install.sh
@@ -1,6 +1,9 @@
+set -x
 wget -q https://github.com/devops-works/binenv/releases/download/v0.19.11/binenv_linux_amd64
 wget -q https://github.com/devops-works/binenv/releases/download/v0.19.11/checksums.txt
 sha256sum --check --ignore-missing checksums.txt
+mkdir -p "$HOME/.config/binenv"
+test -z "${URL_BINENV_DISTRIS:-}" || wget -O - -q "$URL_BINENV_DISTRIS" | grep '^ ' >>$HOME/.config/binenv/distributions.yaml
 mv binenv_linux_amd64 binenv
 chmod +x binenv
 ./binenv update
@@ -9,11 +12,10 @@ rm binenv
 rm checksums.txt
 if [[ -n $BASH ]]; then ZESHELL=bash; fi
 if [[ -n $ZSH_NAME ]]; then ZESHELL=zsh; fi
-test -z "${URL_BINENV_DISTRIS:-}" || wget -O - -q "$URL_BINENV_DISTRIS" | grep '^ ' >>$HOME/.config/binenv/distributions.yaml
 type binenv 2>/dev/null || {
     echo -e '\nexport PATH=~/.binenv:$PATH' >>~/".${ZESHELL}rc"
     . ~/".${ZESHELL}rc"
 }
 test -z "${BINENV_TOOLS:-}" || eval "binenv install $BINENV_TOOLS"
-
-#echo "source <(binenv completion ${ZESHELL})" >>~/".${ZESHELL}rc"
+echo "binenv installed"
+set +x
