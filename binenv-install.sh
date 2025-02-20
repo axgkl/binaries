2090fda43cc49894c0dbdc6943014b6df06d01b5
Gunther Klessinger <gunther.klessinger@axiros.com>
2025-02-20 13:24:45 +0100

    new tools with on demand versions fetcher
    to not overuse github api

--- binenv-install.sh
+++ binenv-install.sh
@@ -1,9 +1,7 @@
-set -x
 wget -q https://github.com/devops-works/binenv/releases/download/v0.19.11/binenv_linux_amd64
 wget -q https://github.com/devops-works/binenv/releases/download/v0.19.11/checksums.txt
 sha256sum --check --ignore-missing checksums.txt
 mkdir -p "$HOME/.config/binenv"
-test -z "${URL_BINENV_DISTRIS:-}" || wget -O - -q "$URL_BINENV_DISTRIS" | grep '^ ' >>$HOME/.config/binenv/distributions.yaml
 mv binenv_linux_amd64 binenv
 chmod +x binenv
 ./binenv update
@@ -12,10 +10,29 @@ rm binenv
 rm checksums.txt
 if [[ -n $BASH ]]; then ZESHELL=bash; fi
 if [[ -n $ZSH_NAME ]]; then ZESHELL=zsh; fi
+# adding patches to distributions.yaml:
+# When there is a NEW one, then there is no version info in the cache - but can be supplied by the user
+test -z "${URL_BINENV_DISTRIS:-}" || wget -O - -q "$URL_BINENV_DISTRIS" | grep '^ ' >>$HOME/.config/binenv/distributions.yaml
 type binenv 2>/dev/null || {
     echo -e '\nexport PATH=~/.binenv:$PATH' >>~/".${ZESHELL}rc"
     . ~/".${ZESHELL}rc"
 }
-test -z "${BINENV_TOOLS:-}" || eval "binenv install $BINENV_TOOLS"
-echo "binenv installed"
-set +x
+set -x
+if [[ -z $BINENV_TOOLS ]]; then
+    prev=""
+    for item in $BINENV_TOOLS x; do
+        if [[ $item != *.* && $prev != *.* && -n $prev ]]; then
+            echo "No version for tool: $prev"
+            # no fail exit code for failed versions, need to do grep output:
+            test "$(binenv versions "$prev" | grep "$prev" | cut -d : -f 2 | tr -d ' ')X" == "X" && {
+                echo "No version in cache - tool unknown to binenv standard: $prev"
+                binenv update -f "$prev"
+            }
+        fi
+        prev="$item"
+    done
+
+    set +x
+    eval "binenv install $BINENV_TOOLS"
+fi
+echo "done tools"
